e26aa4dedacada4629e5790d657e96bc135dac9b
Casey Raethke <raethkcj@uwec.edu>
2017-03-06 14:38:09 -0600

    Begin to write TurnRight

--- src/edu/uwec/cs/robotics/TurnRight.java
+++ src/edu/uwec/cs/robotics/TurnRight.java
@@ -1,19 +1,28 @@
 package edu.uwec.cs.robotics;
 
+import lejos.hardware.port.SensorPort;
+import lejos.hardware.sensor.EV3TouchSensor;
+import lejos.hardware.sensor.SensorModes;
+import lejos.robotics.SampleProvider;
 import lejos.robotics.subsumption.Behavior;
 
 public class TurnRight implements Behavior {
+	private SensorModes sensor = new EV3TouchSensor(SensorPort.S2);
+	private SampleProvider touch = sensor.getMode("Touch");
 
 	@Override
 	public boolean takeControl() {
-		// TODO Auto-generated method stub
-		return false;
+		float[] sample = new float[touch.sampleSize()];
+		touch.fetchSample(sample, 0);
+		return sample[0] == 1f;
 	}
 
 	@Override
 	public void action() {
-		// TODO Auto-generated method stub
-
+		// Back up (to avoid turning into the wall)
+		WallFollower.driver.forward(-200, 300);
+		// Turn right
+		WallFollower.driver.turn(0, 90, false, 300);
 	}
 
 	@Override
--- src/edu/uwec/cs/robotics/WallFollower.java
+++ src/edu/uwec/cs/robotics/WallFollower.java
@@ -4,7 +4,10 @@ import lejos.robotics.subsumption.Arbitrator;
 import lejos.robotics.subsumption.Behavior;
 
 public class WallFollower {
+	public static DiffDriver driver;
+
 	public static void main(String[] args) {
+		driver = new DiffDriver(121, 56);
 		Behavior[] behaviors = { new Follow(), new TurnRight(), new TurnLeft() };
 		Arbitrator arbitrator = new Arbitrator(behaviors);
 		arbitrator.go();
