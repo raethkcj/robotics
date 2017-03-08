886f9de5c012ce4f23ee6a8f72c15b9a1a6fdb55
Casey Raethke <raethkcj@uwec.edu>
2017-03-08 10:29:42 -0600

    Add ultrasonic sensor to Follow

--- src/edu/uwec/cs/robotics/Follow.java
+++ src/edu/uwec/cs/robotics/Follow.java
@@ -1,8 +1,16 @@
 package edu.uwec.cs.robotics;
 
+import java.util.Arrays;
+
+import lejos.hardware.port.SensorPort;
+import lejos.hardware.sensor.EV3UltrasonicSensor;
+import lejos.hardware.sensor.SensorModes;
+import lejos.robotics.SampleProvider;
 import lejos.robotics.subsumption.Behavior;
 
 public class Follow implements Behavior {
+	private SensorModes sensor = new EV3UltrasonicSensor(SensorPort.S1);
+	private SampleProvider distance = sensor.getMode("Distance");
 
 	@Override
 	public boolean takeControl() {
@@ -20,6 +28,15 @@ public class Follow implements Behavior {
 	public void suppress() {
 		// TODO Auto-generated method stub
 
+	}
+
+	public static void main(String[] args) {
+		Follow f = new Follow();
+		float[] sample = new float[f.distance.sampleSize()];
+		while (true) {
+			f.distance.fetchSample(sample, 0);
+			System.out.println(Arrays.toString(sample));
+		}
 	}
 
 }
