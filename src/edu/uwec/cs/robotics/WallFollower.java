28f5e1f88a5187f217ac7876b0f1bfd2b408cd80
Casey Raethke <raethkcj@uwec.edu>
2017-03-08 23:36:30 -0600

    Add exit on enter keypress

--- src/edu/uwec/cs/robotics/WallFollower.java
+++ src/edu/uwec/cs/robotics/WallFollower.java
@@ -1,5 +1,8 @@
 package edu.uwec.cs.robotics;
 
+import lejos.hardware.Button;
+import lejos.hardware.Key;
+import lejos.hardware.KeyListener;
 import lejos.robotics.subsumption.Arbitrator;
 import lejos.robotics.subsumption.Behavior;
 
@@ -9,6 +12,18 @@ public class WallFollower {
 	public static void main(String[] args) {
 		driver = new DiffDriver(121, 56);
 		Behavior[] behaviors = { new Follow(), new TurnRight(), new TurnLeft() };
+
+		Button.ENTER.addKeyListener(new KeyListener() {
+			@Override
+			public void keyReleased(Key k) {
+			}
+
+			@Override
+			public void keyPressed(Key k) {
+				System.exit(0);
+			}
+		});
+
 		Arbitrator arbitrator = new Arbitrator(behaviors);
 		arbitrator.go();
 	}
